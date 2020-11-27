0717005d397736419264ae37051e0b65cbdc7a8b
Ubuntu <ubuntu@ip-172-31-9-144.ec2.internal>
2020-11-27 07:44:00 +0000

    Add review  list screen

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+  has_many :book_reviews
   validates :name, presence: true
   validates :age, presence: true
+  
 end
